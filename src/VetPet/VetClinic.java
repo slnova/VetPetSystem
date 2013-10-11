869cdb565efeeb03dcaf12622b8e44293dbca2dd
Stuart <slnova@hotmail.com>
2013-10-10 18:46:45 -0600

    Refactoring #4 - Duplicate Code and Documentation
    I had commented out the method getAnimalInformation when i was doing the
    third refactoring because i wanted to work on and test the first listing
    method before working on the broken second one.  When i went back to
    make it work i relised that there was no calling code this method at
    all.  I loaded up the original source code, deleted this method and ran
    the code to confirm that it was never used.  Since i was removing an
    entire method anyways i decided to clean up the comments in the same
    file since some of it was no longer relevant to the program.

--- src/VetPet/VetClinic.java
+++ src/VetPet/VetClinic.java
@@ -49,17 +49,10 @@ public class VetClinic
 		return true;		
 	}
     
-    //---------------------------------------------------------------------------
-    /**
-       \brief adds a dog to the clinic 
-       \param[in] name the name of the dog
-       \param[in] ID identification number of the dog (MUST be exactly 6 digits long)
-       \param[in] K9_family family branch the dog is from
-       \param[in] type the classification of the dog
-       
-       \return true is dog has been added, false if the ID number is inncorect
-     */
-    //---------------------------------------------------------------------------
+                
+    //return true is dog has been added, false if the ID number is inncorect
+     
+    
     public boolean addDog(String ID,String type, String name,  String K9_family )
 	{
 
@@ -75,16 +68,9 @@ public class VetClinic
 	}
     
     
-    //---------------------------------------------------------------------------
-    /**
-       \brief adds a cat to the clinic 
-       \param[in] name the name of the cat
-       \param[in] ID identification number of the cat (MUST be exactly 6 digits long)
-       \param[in] toy fav toy of the cat
-       \param[in] type the classification of the cat
-       
-       \return true is cat has been added, false if the ID number is inncorect
-     */
+          	       
+    //return true is cat has been added, false if the ID number is inncorect
+     
     public boolean addCat(String ID, String type, String name, String toy )
     {
     	if(!validID(ID))
@@ -121,47 +107,10 @@ public class VetClinic
 		}
 
 		return("Animal: Cats\n" + catString + "\nAnimal: Dogs\n" + dogString);
-	    }
+    }
 
 	
-    
-    //---------------------------------------------------------------------------
-    /**
-       \brief returns the information on a single animal
-       \return null if the animal could not be found,otherwise a string with the animals information
-     */
-    /*public String getAnimalInformation(String ID)
-    {
-	int i=0;
-	if(ID.length() != 6 ) return null;
-	
-	for(i=0; i < ID.length(); i++ )
-	    if( !Character.isDigit(ID.charAt(i))) return null; 
-
-
-	
-	for(i=0;i< numCats; i++)
-	    {
-		Cat c = Catlist[i];
-
-		if(c.ID.equals(ID))
-		    return "(CAT) ID: "+ c.ID +" Name: " + c.getName() + " Type: " + c.type 
-		               +" Toy: " + c.getFav_cat_toy() +"\n";
-	    }
-		
-
-	for(i=0;i< numDogs; i++)
-	    {
-		Dog d = Doglist[i];
-		
-		if(d.ID.equals(ID))
-		    return "(DOG) ID: "+ d.ID +" Name: " + d.getName() + " Type: " + d.type 
-			+" K9_family: " + d.getK9_family() +"\n";
-	    }
-
-	return null;*/
-   //}
-    //---------------------------------------------------------------------------
+   
     private Cat Catlist[] = new Cat[10]; /**< \brief list of all cats in the clinic */
     private Dog Doglist[] = new Dog[10]; /**< \brief list of all dogs in the clinic */
     private int numDogs = 0; /**< \brief number of dogs */
